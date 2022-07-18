24fdcc30d5db336ffa3f36d99e1134614fd1eb75
SamSepi0l-H3ll0-Fi3ND <94652515+SamSepi0l-H3ll0-Fi3ND@users.noreply.github.com>
2022-07-18 11:37:26 +0200

    Add files via upload

---
+++ components/RegistrationProgress.js
@@ -0,0 +1,12 @@
+const RegistrationProgress = ({}) => {
+  return (
+    <ul class="steps">
+      <li class="step step-primary">Username</li>
+      <li class="step step-primary">Name</li>
+      <li class="step step-primary">Email</li>
+      <li class="step ">Password</li>
+    </ul>
+  );
+};
+
+export default RegistrationProgress;
