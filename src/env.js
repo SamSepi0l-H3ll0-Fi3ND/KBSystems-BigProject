598796c7a499f40dfd2429f8e7ef0efdb5c2a1fb
SamSepi0l-H3ll0-Fi3ND <94652515+SamSepi0l-H3ll0-Fi3ND@users.noreply.github.com>
2022-08-02 10:05:04 +0200

    Delete src directory

--- src/env.js
+++
@@ -1,6 +0,0 @@
-const API = "https://kb.unuel.eu";
-
-// const API = "http://unuel.eu:8000";
-// const API = "http://192.168.0.125:8000";
-
-export default API;
--- src/ui/Tag.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Tag = ({ name }) => {
-  return <div className="btn btn-outline ml-1 btn-xs">{name}</div>;
-};
-
-export default Tag;
